4553e09c3bb0a5a4ba7e59cfbb0b0c8588f70b60
Patricia Teocan <teocan.patricia03@gmail.com>
2026-04-30 15:40:58 +0300

    Add main menu fields

--- react-chat/src/components/mainMenu/MainMenu.tsx
+++ react-chat/src/components/mainMenu/MainMenu.tsx
@@ -1,6 +1,23 @@
 import "./MainMenu.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faComment,
+  faCommentDots,
+  type IconDefinition,
+} from "@fortawesome/free-solid-svg-icons";
+import { faBoxArchive } from "@fortawesome/free-solid-svg-icons/faBoxArchive";
 
 export const MainMenu = () => {
+  const displayFieldItem = (title: string, icon: IconDefinition) => {
+    return (
+      <button className="menu__fields__item">
+        <FontAwesomeIcon icon={icon} className="menu__fields__item-icon" />
+        <p>{title}</p>
+      </button>
+    );
+  };
+
   return (
     <div className="menu">
       <div className="menu__header">
@@ -10,6 +27,12 @@ export const MainMenu = () => {
           alt="Current user profile picture"
         />
         <div className="menu__header-name">John</div>
+      </div>
+      <div className="menu__fields">
+        {displayFieldItem("Chats", faComment)}
+        {displayFieldItem("Calls", faPhone)}{" "}
+        {displayFieldItem("Message requests", faCommentDots)}{" "}
+        {displayFieldItem("Archive", faBoxArchive)}
       </div>
     </div>
   );
