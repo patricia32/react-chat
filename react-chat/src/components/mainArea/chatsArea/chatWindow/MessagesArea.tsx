a3e2e28b057a4c0d9162a09681725863da9d7c14
Patricia Teocan <teocan.patricia03@gmail.com>
2026-05-21 15:24:51 +0300

    (MessagesArea): display date details

--- react-chat/src/components/mainArea/chatsArea/chatWindow/MessagesArea.tsx
+++ react-chat/src/components/mainArea/chatsArea/chatWindow/MessagesArea.tsx
@@ -2,6 +2,8 @@ import { useEffect, useRef } from "react";
 import type { Message } from "../../../../models/chat";
 import { MessageBox } from "./MessageBox";
 import "./MessagesArea.scss";
+import { formatMessageDate } from "../../../../utils/functions";
+import React from "react";
 
 interface MessagesAreaProps {
   messages: Message[];
@@ -16,9 +18,26 @@ export const MessagesArea = ({ messages }: MessagesAreaProps) => {
 
   return (
     <div className="messages" ref={messagesRef}>
-      {messages.map((message) => (
-        <MessageBox key={message.messageId} message={message} />
-      ))}
+      {messages.map((message, index) => {
+        const currentDate = new Date(message.createdAt);
+
+        const previousMessage = messages[index - 1];
+        const previousDate = previousMessage
+          ? new Date(previousMessage.createdAt)
+          : null;
+
+        const showDate =
+          !previousDate ||
+          previousDate.toDateString() !== currentDate.toDateString();
+        return (
+          <React.Fragment key={message.messageId}>
+            <div className="messages-date">
+              {showDate && formatMessageDate(message.createdAt)}
+            </div>
+            <MessageBox key={message.messageId} message={message} />
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 };
--- react-chat/src/utils/functions.ts
+++ react-chat/src/utils/functions.ts
@@ -17,3 +17,38 @@ export const getSecondUser = async (
     });
   }
 };
+
+export const formatMessageDate = (dateProp: Date) => {
+  const date = new Date(dateProp);
+  const today = new Date();
+
+  const yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+
+  const isToday =
+    date.getDate() === today.getDate() &&
+    date.getMonth() === today.getMonth() &&
+    date.getFullYear() === today.getFullYear();
+
+  if (isToday) return "Today";
+
+  const isYesterday =
+    date.getDate() === yesterday.getDate() &&
+    date.getMonth() === yesterday.getMonth() &&
+    date.getFullYear() === yesterday.getFullYear();
+
+  if (isYesterday) return "Yesterday";
+
+  const diffTime = today.getTime() - date.getTime();
+  const diffDays = diffTime / (1000 * 60 * 60 * 24);
+
+  if (diffDays < 7)
+    return date.toLocaleDateString("en-US", {
+      weekday: "long",
+    });
+
+  return date.toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "long",
+  });
+};
