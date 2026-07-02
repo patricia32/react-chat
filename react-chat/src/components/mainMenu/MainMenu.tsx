ef8cbb82139f0865c616da5c9b9a07660a4af7ad
Patricia Teocan <teocan.patricia03@gmail.com>
2026-07-02 11:36:30 +0300

    feat: MainArea responsiveness

--- react-chat/src/components/mainMenu/MainMenu.tsx
+++ react-chat/src/components/mainMenu/MainMenu.tsx
@@ -5,28 +5,32 @@ import { faBoxArchive } from "@fortawesome/free-solid-svg-icons/faBoxArchive";
 import {
   faPhone,
   faComment,
-  faCommentDots,
   type IconDefinition,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useAppStore } from "../../store/appStore";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 export const MainMenu = () => {
   const selectedField = useAppStore((state) => state.selectedField);
   const setSelectedField = useAppStore((state) => state.setSelectedField);
+  const [openMenu, setOpenMenu] = useState(false);
 
   const selectField = (field: string) => {
     if (selectedField === field) setSelectedField("");
     else setSelectedField(field);
   };
 
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   const displayFieldItem = (field: string, icon: IconDefinition) => {
     return (
       <button
         onClick={() => selectField(field)}
-        className={`menu__fields__item ${
-          field === selectedField ? "active" : ""
-        }`}
+        className={`item ${field === selectedField ? "active" : ""}`}
       >
         <div className="icon">
           <FontAwesomeIcon icon={icon} className="icon-item" />
@@ -38,7 +42,7 @@ export const MainMenu = () => {
 
   return (
     <div className="menu">
-      <div className="menu__header">
+      <div className={`menu__header ${openMenu ? "mobile" : ""}`}>
         <img
           className="menu__header-image"
           src="usersPhotos/primary.png"
@@ -46,12 +50,21 @@ export const MainMenu = () => {
         />
         <div className="menu__header-name">John</div>
       </div>
+      <div className="menu__hamburger" onClick={toggleMenu}>
+        <MenuIcon />
+      </div>
       <div className="menu__fields">
         {displayFieldItem("Chats", faComment)}
         {displayFieldItem("Calls", faPhone)}{" "}
-        {displayFieldItem("Message requests", faCommentDots)}{" "}
         {displayFieldItem("Archive", faBoxArchive)}
       </div>
+      {/* <div className={`${openMenu ? "open" : "close"}`}> */}
+      <div className={`menu__fieldsMobile ${openMenu ? "open" : "close"}`}>
+        {displayFieldItem("Chats", faComment)}
+        {displayFieldItem("Calls", faPhone)}{" "}
+        {displayFieldItem("Archive", faBoxArchive)}
+      </div>
+      {/* </div> */}
     </div>
   );
 };
